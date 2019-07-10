729ce7f0a5a8940267d6ccf1b84880c73c39f86d
George Theofilis <g.theofilis@clmsuk.com>
2019-07-10 10:15:10 +0300

    Include OWIN

---
+++ src/CLMS.Framework/Owin/BasicAuthenticationHandler.cs
@@ -0,0 +1,80 @@
+#if NETFRAMEWORK
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+
+namespace CLMS.Framework.Owin
+{
+    internal class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions>
+    {
+        private readonly string _challenge;
+
+        public BasicAuthenticationHandler(BasicAuthenticationOptions options)
+        {
+            _challenge = "Basic realm=" + options.Realm;
+        }
+
+        protected override async Task<AuthenticationTicket> AuthenticateCoreAsync()
+        {
+            var authzValue = Request.Headers.Get("Authorization");
+            if (string.IsNullOrEmpty(authzValue) || !authzValue.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
+            {
+                return null;
+            }
+            var token = authzValue.Substring("Basic ".Length).Trim();
+            var claims = await TryGetPrincipalFromBasicCredentials(token, Options.CredentialValidationFunction);
+            if (claims == null)
+            {
+                return null;
+            }
+            var id = new ClaimsIdentity(claims, Options.AuthenticationType);
+            return new AuthenticationTicket(id, new AuthenticationProperties());
+        }
+
+        protected override Task ApplyResponseChallengeAsync()
+        {
+            if (Response.StatusCode == 401)
+            {
+                var challenge = Helper.LookupChallenge(Options.AuthenticationType, Options.AuthenticationMode);
+                if (challenge != null)
+                {
+                    Response.Headers.AppendValues("WWW-Authenticate", _challenge);
+                }
+            }
+            return Task.FromResult<object>(null);
+        }
+
+        async Task<IEnumerable<Claim>> TryGetPrincipalFromBasicCredentials(string credentials,
+                BasicAuthenticationMiddleware.CredentialValidationFunction validate)
+        {
+            string pair;
+            try
+            {
+                pair = Encoding.UTF8.GetString(
+                           Convert.FromBase64String(credentials));
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+            var ix = pair.IndexOf(':');
+            if (ix == -1)
+            {
+                return null;
+            }
+            var username = pair.Substring(0, ix);
+            var pw = pair.Substring(ix + 1);
+            return await validate(username, pw);
+        }
+    }
+}
+#else
+#endif
