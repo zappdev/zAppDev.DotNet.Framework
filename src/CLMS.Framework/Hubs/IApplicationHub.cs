441fb3d026b80482854037bc36d9c4db89b0f4a9
Tolis <apostolis944@gmail.com>
2019-07-11 16:56:10 +0300

    Identity Helper updates

--- src/CLMS.Framework/Hubs/IApplicationHub.cs
+++ src/CLMS.Framework/Hubs/IApplicationHub.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CLMS.Framework.Hubs
 {
@@ -10,5 +11,8 @@ namespace CLMS.Framework.Hubs
         void RaiseSessionStartEvent(string _groupName = null);
         void RaiseOnInstanceSaveEvent(object Instance, string _groupName = null);
         void RaiseFileDownloadEvent(string path, string username, string _groupName = null);
+        void RaiseSignOut(string username, DateTime time);
+        void ForceUserPageReload(string user);
+        List<string> GetAllConnectedUsers();
     }
 }
