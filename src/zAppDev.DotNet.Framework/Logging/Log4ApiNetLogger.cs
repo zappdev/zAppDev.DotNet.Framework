4e1693becb3e1933f6ef2091d02a8924bac3a2e5
George Theofilis <g.theofilis@clmsuk.com>
2019-10-09 14:31:48 +0300

    Simple Log4Net logger

--- src/zAppDev.DotNet.Framework/Logging/Log4ApiNetLogger.cs
+++ src/zAppDev.DotNet.Framework/Logging/Log4ApiNetLogger.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿#if NETFRAMEWORK
+#else
+using Microsoft.Extensions.Logging;
+using System;
+#endif
 
 namespace zAppDev.DotNet.Framework.Logging
 {
@@ -6,9 +10,16 @@ namespace zAppDev.DotNet.Framework.Logging
 #else
     public class Log4ApiNetLogger : IAPILogger
     {
+        private readonly ILogger _logger;
+
+        public Log4ApiNetLogger (ILogger<Log4ApiNetLogger> logger)
+        {
+            _logger = logger;
+        }
+
         public void LogExposedAPIAccess(string controller, string action, Guid requestId, TimeSpan processingTime, bool cacheHit)
         {
-            
+            _logger.LogInformation($"{controller},{action},{requestId},{processingTime},{cacheHit}");
         }
     }
 #endif
