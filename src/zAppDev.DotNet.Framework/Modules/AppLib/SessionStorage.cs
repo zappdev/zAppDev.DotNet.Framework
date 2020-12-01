c7e9b13901eb2290e4baea1f31b356adbdf3c9d7
George Theofilis <theofilis.g@gmail.com>
2020-12-01 10:51:16 +0200

    Add new Session object

---
+++ src/zAppDev.DotNet.Framework/Modules/AppLib/SessionStorage.cs
@@ -0,0 +1,62 @@
+﻿#if NETFRAMEWORK
+using System.Web;
+#else
+using Microsoft.AspNetCore.Http;
+#endif
+
+
+namespace zAppDev.DotNet.Framework.Modules.AppLib
+{
+    public class SessionStorage
+    {
+        private HttpContext _httpContext;
+
+        public SessionStorage(HttpContext httpContext)
+        {
+            _httpContext = httpContext;
+        }
+
+        public void Add(string key, object value)
+        {
+#if NETFRAMEWORK
+            _httpContext.Session.Add(key, value);
+#else
+            _httpContext.Session.Set(key, Utilities.BinarySerializer.Serialize(value));
+#endif
+        }
+
+        public object Get(string key)
+        {
+#if NETFRAMEWORK
+            return _httpContext.Session[key];
+#else
+            var result = _httpContext.Session.Get(key);
+            return Utilities.BinarySerializer.Desserialize(result);
+#endif
+        }
+
+        public void Remove(string key)
+        {
+#if NETFRAMEWORK
+            _httpContext.Session.Remove(key);
+#else
+            _httpContext.Session.Remove(key);
+#endif
+        }
+
+        public void Clear()
+        {
+#if NETFRAMEWORK
+            _httpContext.Session.Clear();
+#else
+            _httpContext.Session.Clear();
+#endif
+        }
+
+        public object this[string key]
+        {
+            get => Get(key);
+            set => Add(key, value);
+        }
+    }
+}
---
+++ src/zAppDev.DotNet.Framework/Utilities/BinarySerializer.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace zAppDev.DotNet.Framework.Utilities
+{
+    public class BinarySerializer
+    {
+        public static byte[] Serialize(object obj)
+        {
+            if (obj == null)
+                return null;
+            var bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        public static object Desserialize(byte[] bytes)
+        {
+            if (bytes == null)
+                return default;
+            var bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                var obj = bf.Deserialize(ms);
+                return obj;
+            }
+        }
+    }
+}
