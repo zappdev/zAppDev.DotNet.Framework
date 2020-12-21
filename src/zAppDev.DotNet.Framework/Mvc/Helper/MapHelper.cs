daabb44160e8b82c2c38e06115cd726104dfb975
George Theofilis <theofilis.g@gmail.com>
2020-12-21 13:24:10 +0200

    Revert change

--- src/zAppDev.DotNet.Framework/Mvc/Helper/MapHelper.cs
+++ src/zAppDev.DotNet.Framework/Mvc/Helper/MapHelper.cs
@@ -106,8 +106,8 @@ namespace zAppDev.DotNet.Framework.Mvc
 
     public class MapHelperCoordinates
     {
-        public decimal? Latitude { get; set; }
-        public decimal? Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
     }
 
     public class GeoJsonObject
