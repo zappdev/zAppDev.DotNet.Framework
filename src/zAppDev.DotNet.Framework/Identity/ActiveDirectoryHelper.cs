f185becee79e718df40502f59373278d0d1446d7
Dimitris Moudilos <d.moudilos@clmsuk.com>
2021-03-31 16:42:03 +0300

    Merge AD helper from legacy

--- src/zAppDev.DotNet.Framework/Identity/ActiveDirectoryHelper.cs
+++ src/zAppDev.DotNet.Framework/Identity/ActiveDirectoryHelper.cs
@@ -6,11 +6,14 @@ using System.Linq;
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
+using System.Security.Principal;
 
 namespace zAppDev.DotNet.Framework.Identity
 {
     public class ActiveDirectoryHelper
     {
+        private static ILog _logger = log4net.LogManager.GetLogger(typeof(ActiveDirectoryHelper));
+
         public static List<string> GetAllUsers(string domain)
         {
             var users = new List<string>();
@@ -53,27 +56,79 @@ namespace zAppDev.DotNet.Framework.Identity
             return users;
         }
 
-        public static string GetCurrentUserProperty(string domain, string key)
+        public static string GetCurrentUserProperty(string domain, string key, string group = null)
         {
-            return GetUserProperty(IdentityHelper.GetCurrentUserName(), domain, key);
+            return GetUserProperty(IdentityHelper.GetCurrentUserName(), domain, key, group);
         }
 
-        public static string GetUserProperty(string userName, string domain, string key)
+        public static string GetUserProperty(string userName, string domain, string key, string group = null)
         {
-            using (var context = new PrincipalContext(ContextType.Domain, domain))
+            var result = GetUserProperties(userName, domain, new List<string>() { key }, group);
+            if (result == null) return null;
+            return result[key]?.ToString();
+        }
+
+        private static void FillUserProperties(UserPrincipal user, Dictionary<string, object> results, List<string> keys = null)
+        {
+            _logger.Debug("FillUserProperties: Filling in user's properties");
+
+            var fetchAll = keys?.Any() != true;
+
+            var properties = user.GetType().GetProperties();
+            foreach (var property in properties)
             {
-                PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
-                UserPrincipal user = UserPrincipal.FindByIdentity(ctx, NormalizeUsername(userName));
-                if (user == null)
+                _logger.Debug($"FillUserProperties: Examining [{property.Name}]");
+                if (fetchAll || keys.FirstOrDefault(x => string.CompareOrdinal(x, property.Name) == 0) != null)
                 {
-                    LogManager.GetLogger(typeof(ActiveDirectoryHelper))
-                    .Debug($"User '{userName}'(normalized: '{NormalizeUsername(userName)}) not found under '{domain}' domain");
-                    return "";
+                    var value = property.GetValue(user, null);
+                    results[property.Name] = value;
+                    _logger.Debug($"FillUserProperties: [{property.Name} = {value}]");
                 }
-                DirectoryEntry directoryEntry = user.GetUnderlyingObject() as DirectoryEntry;
-                return directoryEntry.Properties[key].Value?.ToString();
             }
         }
+
+        private static string GetUserPrimaryGroup(DirectoryEntry de)
+        {
+            _logger.Debug("GetUserPrimaryGroup: Getting user's Primary Group");
+
+            _logger.Debug("GetUserPrimaryGroup: Refreshing Cache with primaryGroupID and objectSid");
+            de.RefreshCache(new[] { "primaryGroupID", "objectSid" });
+
+            //Get the user's SID as a string
+            var sid = new SecurityIdentifier((byte[])de.Properties["objectSid"].Value, 0).ToString();
+            _logger.Debug($"GetUserPrimaryGroup: Fetched [sid = {sid}]");
+
+            //Replace the RID portion of the user's SID with the primaryGroupId
+            //so we're left with the group's SID
+            sid = sid.Remove(sid.LastIndexOf("-", StringComparison.Ordinal) + 1);
+            sid = sid + de.Properties["primaryGroupId"].Value;
+            _logger.Debug($"GetUserPrimaryGroup: Created [sid = {sid}]");
+
+
+            //Find the group by its SID
+            _logger.Debug($"GetUserPrimaryGroup: Finding the group by its SID, refreshing cn");
+            var group = new DirectoryEntry($"LDAP://<SID={sid}>");
+            group.RefreshCache(new[] { "cn" });
+
+            var cn = group.Properties["cn"].Value as string;
+            _logger.Debug($"GetUserPrimaryGroup: [cn = {cn}]");
+            return cn;
+        }
+
+        private static bool PropertyAdded(Dictionary<string, object> properties, string name)
+        {
+            foreach (var key in properties.Keys)
+            {
+                if (string.CompareOrdinal(key.Trim(), name.Trim()) == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
 
         /// <summary>
         /// Returns a collection of a given User's properties
@@ -82,28 +137,120 @@ namespace zAppDev.DotNet.Framework.Identity
         /// <param name="domain">The Domain name</param>
         /// <param name="keys">The properties to be fetched. If null or empty, all properties will be retrieved</param>
         /// <returns></returns>
-        public static Dictionary<string, object> GetUserProperties(string userName, string domain, List<string> keys = null)
+        public static Dictionary<string, object> GetUserProperties(string userName, string domain, List<string> keys = null, string group = null)
         {
             using (var context = new PrincipalContext(ContextType.Domain, domain))
             {
-                PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
-                UserPrincipal user = UserPrincipal.FindByIdentity(ctx, NormalizeUsername(userName));
+                _logger.Debug($"GetUserProperties: Getting user's properties");
+                UserPrincipal user = null;
+                DirectoryEntry userDirectoryEntry = null;
+                if (string.IsNullOrWhiteSpace(group))
+                {
+                    _logger.Debug($"GetUserProperties: Searching for User inside his/her Domain");
+                    PrincipalContext ctx = new PrincipalContext(ContextType.Domain);
+                    user = UserPrincipal.FindByIdentity(ctx, NormalizeUsername(userName));
+
+                    //OK this is an obvious overkill, but I don't know what else to do 
+                    if (user == null)
+                    {
+                        _logger.Debug($"GetUserProperties: Didn't find the user. Searching within all Directory Entries");
+                        using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
+                        {
+                            foreach (var result in searcher.FindAll())
+                            {
+                                var de = result.GetUnderlyingObject() as DirectoryEntry;
+                                de.RefreshCache(new string[] { "samAccountName" });
+                                var samAccountName = de.Properties["samAccountName"].Value?.ToString();
+                                if (string.CompareOrdinal(samAccountName, userName) == 0)
+                                {
+                                    userDirectoryEntry = de;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    _logger.Debug($"GetUserProperties: Searching for User inside his/her group: {group}");
+                    GroupPrincipal grp = GroupPrincipal.FindByIdentity(context, group);
+                    if (grp != null)
+                    {
+                        foreach (Principal p in grp.GetMembers())
+                        {
+                            if (p is UserPrincipal)
+                            {
+                                var userPrincipal = p as UserPrincipal;
+                                if (string.CompareOrdinal(userPrincipal.SamAccountName, userName) == 0)
+                                    user = userPrincipal;
+                            }
+                        }
+                    }
+                }
+
+
                 var userProperties = new Dictionary<string, object>();
-                if (user == null)
+                if (user == null && userDirectoryEntry == null)
                 {
                     LogManager.GetLogger(typeof(ActiveDirectoryHelper))
-                    .Debug($"User '{userName}'(normalized: '{NormalizeUsername(userName)}) not found under '{domain}' domain");
+                    .Error($"User '{userName}'(normalized: '{NormalizeUsername(userName)}) not found under '{domain}' domain");
                     return userProperties;
                 }
-                DirectoryEntry directoryEntry = user.GetUnderlyingObject() as DirectoryEntry;
+
+                _logger.Debug($"GetUserProperties: Found user [{userName}] in AD");
+
+                DirectoryEntry directoryEntry = null;
+                if (user != null)
+                    directoryEntry = user.GetUnderlyingObject() as DirectoryEntry;
+                else
+                    directoryEntry = userDirectoryEntry;
+
+
+                _logger.Debug($"GetUserProperties: Refreshing Cache");
+                directoryEntry.RefreshCache();
+
                 bool fetchAll = keys?.Any() != true;
+
+                if (fetchAll)
+                    directoryEntry.RefreshCache();
+                else
+                {
+                    foreach (var key in keys)
+                    {
+                        _logger.Debug($"GetUserProperties: Refreshing cache for key [{key}]");
+                        directoryEntry.RefreshCache(new string[] { key });
+                    }
+                }
+
+                _logger.Debug($"GetUserProperties: Filling user properties");
+                FillUserProperties(user, userProperties, keys);
+
+
+                if ((fetchAll || keys.FirstOrDefault(x => string.CompareOrdinal(x, "PrimaryGroup") == 0) != null) && (!PropertyAdded(userProperties, "PrimaryGroup")))
+                {
+                    _logger.Debug($"GetUserProperties: Getting user's Primary Group");
+                    userProperties["PrimaryGroup"] = GetUserPrimaryGroup(directoryEntry);
+                }
+
+                _logger.Debug($"GetUserProperties: Browsing through the properties");
                 foreach (PropertyValueCollection property in directoryEntry.Properties)
                 {
                     if (fetchAll || (keys.FirstOrDefault(x => string.Compare(x, property.PropertyName, true) == 0) != null))
                     {
+                        if (PropertyAdded(userProperties, property.PropertyName)) continue;
                         userProperties[property.PropertyName] = property.Value;
+                        _logger.Debug($"GetUserProperties: [{property.PropertyName} = {property.Value}]");
                     }
                 }
+
+                _logger.Debug($"GetUserProperties: Browsing through the keys, as a fail-safe");
+                foreach (var key in keys)
+                {
+                    if (PropertyAdded(userProperties, key)) continue;
+                    var value = directoryEntry.Properties[key]?.Value;
+                    if (value != null) userProperties[key] = value;
+                    _logger.Debug($"GetUserProperties: [{key} = {value}]");
+                }
+
                 return userProperties;
             }
         }
